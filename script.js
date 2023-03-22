27f4ab405c4723c2058c336aaf17aaae50b2882d
pete <pkeen7@gmail.com>
2023-03-21 21:15:01 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('Hello!');
+
+
+/*
+    Aim create random life coach messages
+    Ask user how they are feeling
+        reply accordingly (feeling sad: 'sorry to hear that..')
+    Return random message combining two actions, serious and silly
+    e.g. You should [quit your job] and [join a mariachi band]
+*/
+
+const arrSeriousActions = [
+    'quit your job',
+    'leave your partner',
+    'stop coasting',
+    'run away',
+    'pick yourself up by the bootstraps',
+    'stop people pleasing',
+]
