2cb180cb35c3df16bf44c039f72830cefb4fe0a2
pete <pkeen7@gmail.com>
2023-03-22 18:21:43 -0600

    add message object

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-console.log('Hello!');
-
-
 /*
     Aim create random life coach messages
     Ask user how they are feeling
@@ -9,11 +6,42 @@ console.log('Hello!');
     e.g. You should [quit your job] and [join a mariachi band]
 */
 
-const arrSeriousActions = [
-    'quit your job',
-    'leave your partner',
-    'stop coasting',
-    'run away',
-    'pick yourself up by the bootstraps',
-    'stop people pleasing',
-]
+const message = {
+
+    arrSeriousActions: [
+        'quit your job',
+        'leave your partner',
+        'stop coasting',
+        'run away',
+        'pick yourself up by the bootstraps',
+        'stop people pleasing',
+        'stop wasting time',
+    ],
+
+    arrSillyActions: [
+        'join a mariachi band',
+        'become a stripper in Leeds',
+        'become a door-to-door door salesman',
+        'make the worlds largest bar of soap',
+        'go take mushrooms in Oslo',
+        'become a hamster hairdresser',
+
+    ],
+
+    /*
+    getMood () {
+        const mood = prompt('How are you feeling? (happy, neutral or sad)');
+    },
+    */
+
+    giveAdvice () {
+        const seriousAction = this.arrSeriousActions[Math.floor(Math.random() * this.arrSeriousActions.length)];
+        const sillyAction = this.arrSillyActions[Math.floor(Math.random() * this.arrSillyActions.length)];
+        return `You should ${seriousAction} and ${sillyAction}.`;
+    },
+
+
+}
+
+
+console.log(message.giveAdvice());
